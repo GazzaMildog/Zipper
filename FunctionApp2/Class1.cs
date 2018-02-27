28e7b1a53fcccab560b9dd7f82b497e021407c3f
Gary Miller <gary.miller@daviker.co.uk>
2018-02-27 13:35:24 +0000

    first commit

--- FunctionApp2/Class1.cs
+++ FunctionApp2/Class1.cs
@@ -1,33 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ICSharpCode.SharpZipLib.Zip;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.WindowsAzure.Storage.Auth;
-using System.Net.Http;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Net.Http;
+using System.Text;
 
 namespace Max.RecordingStreamer
 {
     public static class Httpfunction
     {
-
         [FunctionName("HttpTriggerCSharp")]
         //public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
         public static HttpResponseMessage Run(
          HttpRequestMessage req, TraceWriter log)
         {
-        //    var data = await req.Content.ReadAsStringAsync();
-        //    var formValues = data.Split('&')
-        //        .Select(value => value.Split('='))
-        //        .ToDictionary(pair => Uri.UnescapeDataString(pair[0]).Replace("+", " "),
-        //pair => Uri.UnescapeDataString(pair[1]).Replace("+", " "));
-            
+            //    var data = await req.Content.ReadAsStringAsync();
+            //    var formValues = data.Split('&')
+            //        .Select(value => value.Split('='))
+            //        .ToDictionary(pair => Uri.UnescapeDataString(pair[0]).Replace("+", " "),
+            //pair => Uri.UnescapeDataString(pair[1]).Replace("+", " "));
+
             log.Info(String.Format("{0} Sup {1}!", req, "gary"));
             Console.WriteLine("hello");
             //return Task.FromResult<object>(true);
--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -1,16 +1,15 @@
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
-using ICSharpCode.SharpZipLib.Zip;
-using System.IO;
-using ICSharpCode.SharpZipLib.Core;
-using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 
 namespace FunctionApp2
 {
@@ -18,10 +17,9 @@ namespace FunctionApp2
     {
         [FunctionName("Function1")]
         public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "HttpTriggerCSharp/name/{name}/{sas}/{blobname}")]HttpRequestMessage req, string name,
-            string sas, TraceWriter log
-            )
+            string sas, TraceWriter log)
         {
-            sas = "SharedAccessSignature=sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdl&st=2018-02-22T15%3A52%3A00Z&se=2019-02-25T15%3A52%3A00Z&sig=G4eBQmUgE7TfgQclAEpa82DYFjaFZArF%2BuraClQIJC8%3D;BlobEndpoint=https://maxstoretest.blob.core.windows.net/;FileEndpoint=https://maxstoretest.file.core.windows.net/;QueueEndpoint=https://maxstoretest.queue.core.windows.net/;TableEndpoint=https://maxstoretest.table.core.windows.net/;";
+            sas = "";
             log.Info("C# HTTP trigger function processed a request.");
 
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(sas);
@@ -32,62 +30,44 @@ namespace FunctionApp2
             var list = container.ListBlobs();
             var listOfBlobNames = new List<string>();
 
-            CloudBlockBlob uploadblob = container.GetBlockBlobReference("zup.zip");
             MemoryStream outputstream = new MemoryStream();
             ZipOutputStream zipper = new ZipOutputStream(outputstream);
             zipper.SetLevel(3);
-            int fourOfour = 0;
+            CloudAppendBlob uploadblob = container.GetAppendBlobReference("zup.zip");
             foreach (var blob in list)
             {
                 var Blobfilename = blob.Uri.Segments.Last();
                 listOfBlobNames.Add(Blobfilename);
-
             }
-            foreach(var blobname in listOfBlobNames)
+            foreach (var blobname in listOfBlobNames)
             {
-                fourOfour = 0;
-                CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobname);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobname);
+                    try { blockBlob.DownloadToStream(stream); }
+                    catch { }
 
-                using (MemoryStream stream = new MemoryStream()) {
-
-                    try { blockBlob.DownloadToStream(zipper); }
-                    catch
-                    {
-                        fourOfour = 1;
-                    }
-                
-                    if(fourOfour == 0)
-                    {
-                        ZipEntry newEntry = new ZipEntry(blobname);
-                        zipper.PutNextEntry(newEntry);
-                        stream.Position = 0;
-                        StreamUtils.Copy(stream, zipper, new byte[512]);
-                        zipper.CloseEntry();
-                        stream.Close();
-                        //  outputstream.Position = 0;
-                        // uploadblob.UploadFromStream(zipper);
-                    }
-
-                    
+                    ZipEntry newEntry = new ZipEntry(blobname);
+                    zipper.PutNextEntry(newEntry);
+                    stream.Position = 0;
+                    StreamUtils.Copy(stream, zipper, new byte[4096]);
+                    zipper.CloseEntry();
+                    stream.Close();
+                    outputstream.Position = 0;
+                    uploadblob.UploadFromStream(outputstream);
                 }
             }
-           
-           
-
 
             zipper.IsStreamOwner = false;
             zipper.Close();
             outputstream.Position = 0;
 
-            
             uploadblob.UploadFromStream(outputstream);
 
-            
             outputstream.Close();
 
             // Fetching the name from the path parameter in the request URL
             return req.CreateResponse(HttpStatusCode.OK, "Hello ");
         }
-
     }
 }
