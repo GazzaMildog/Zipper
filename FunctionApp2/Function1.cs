3a0326c9e720bce339fe997bcef7f6a109a2b54d
Gary Miller <gary.miller@daviker.co.uk>
2018-02-28 16:50:45 +0000

    Cleaned up code, changed variables etc

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -21,27 +21,37 @@ namespace FunctionApp2
              TraceWriter log)
 
         {
-            // parse query parameter
+            log.Info("Setting up variables...");
 
-            // Get request body
-            // Set name to query string or body data
 
+            // FORMATTING OF SAS - SharedAccessSignature=sv=2017-04-17/ss=*stuff*/srt=*stuff*/sp=*stuff*/st=*stuff*/se=*stuff*/sig=*stuff*/*put the name of the storage account here*
+            // this will be added to the main url in the format shown above
             string sas = (sv + "&" + ss + "&" + srt + "&" + sp + "&" + st + "&" + se + "&" + sig + ";BlobEndpoint=https://" + sAccName + ".blob.core.windows.net/;");
             sas = sas.Replace("++", "%");
 
-            string request = req.Content.Headers.ToString();
-            log.Info("f");
 
+            //variables and initial set up
+
+
+
+
+            string request = req.Content.Headers.ToString();
             foreach (DriveInfo drive in DriveInfo.GetDrives().Where(d => d.IsReady))
             {
                 log.Info($"{drive.Name}: {drive.TotalFreeSpace / 1024 / 1024 / 1024} GB");
             }
-
-
             log.Info("C# HTTP trigger function processed a request.");
 
+            string zipFileName = ("blobzipper last part.zip");
             string containername = "recordings";
             CloudStorageAccount storageAccount = null;
+            int zipTransitionCount = 0;
+            int amountOfParts = 0;
+            System.IO.Directory.CreateDirectory("C:\\Zipper");
+            MemoryStream outputstream = new MemoryStream();
+            ZipOutputStream zipper = new ZipOutputStream(outputstream);
+            zipper.SetLevel(1);
+
 
             try
             {
@@ -56,16 +66,16 @@ namespace FunctionApp2
             CloudBlobContainer container = blobClient.GetContainerReference(containername);
             container.CreateIfNotExists();
 
+
+
+
+            CloudBlockBlob uploadblob = container.GetBlockBlobReference(zipFileName);
+
+            // list variables
+
             var list = container.ListBlobs(useFlatBlobListing: true);
             var listOfBlobNames = new List<string>();
 
-            MemoryStream outputstream = new MemoryStream();
-            ZipOutputStream zipper = new ZipOutputStream(outputstream);
-            zipper.SetLevel(1);
-
-            string zipFileName = ("zup " + DateTime.Now.Millisecond + ".zip");
-
-            CloudBlockBlob uploadblob = container.GetBlockBlobReference(zipFileName);
 
             // this loop constucts a list of blobs that can be downloaded
 
@@ -78,9 +88,12 @@ namespace FunctionApp2
                 listOfBlobNames.Add(correctfilename);
             }
 
+
+
             // this loop adds files into entries in a zip file
             foreach (var blobname in listOfBlobNames)
             {
+                
                 int fourofour = 0;
                 MemoryStream stream = new MemoryStream();
 
@@ -89,12 +102,14 @@ namespace FunctionApp2
                 try { blockBlob.DownloadToStream(stream); }
                 catch
                 {
+
                     fourofour = 1;
                     log.Info("skipping: 404 not found - " + blobname);
                 }
 
-                if (fourofour == 0)
-                {
+                    if (fourofour == 0)
+                    {
+                    ++zipTransitionCount;
                     log.Info("Streaming " + blobname + " to zip " + zipFileName);
                     ZipEntry newEntry = new ZipEntry(blobname);
                     zipper.PutNextEntry(newEntry);
@@ -102,7 +117,28 @@ namespace FunctionApp2
                     stream.CopyTo(zipper);
                     zipper.CloseEntry();
                     stream.Close();
-                }
+
+
+                    // this if statement clears memory by writing the currect amount files to a zip.
+                        if(zipTransitionCount == 100)
+                    {
+                        log.Info("Writing zip file to temp drive");
+                        ++amountOfParts;
+                        zipper.IsStreamOwner = false;
+                        zipper.Close();
+                        var FileSaver = File.Create("C:\\Zipper\\BlobZip Part " + amountOfParts+".zip");
+                        outputstream.Position = 0;
+                        outputstream.CopyTo(FileSaver);
+                        outputstream.Dispose();
+                        outputstream.Close();
+
+                        outputstream = new MemoryStream();
+                        zipper = new ZipOutputStream(outputstream);
+                        zipTransitionCount = 0;
+
+                    }
+                    }
+                    
             }
 
             zipper.IsStreamOwner = false;
